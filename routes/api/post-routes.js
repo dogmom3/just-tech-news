ee9bd807f8f9067597ec855c9b6609a66ee2a1c3
dogmom3 <blair.seivers@yahoo.com>
2022-05-01 12:10:53 -0400

    update get order to desc

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -5,6 +5,7 @@ const { Post, User } = require("../../models");
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: ["id", "post_url", "title", "created_at"],
+    order: [['created_at', 'DESC']],
     include: [
       {
         model: User,
@@ -61,7 +62,7 @@ router.get('/:id', (req, res) => {
   });
 
   //updates existing post
-  router.put('/', (req, res) => {
+  router.put('/:id', (req, res) => {
     Post.update({
           title: req.body.title
         },
@@ -84,5 +85,24 @@ router.get('/:id', (req, res) => {
         });
   })
 
+//delete a post
+  router.delete('/:id', (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
