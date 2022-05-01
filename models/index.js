0ab1cff02ba062579d5bfc7979d99c0bb51c84d9
dogmom3 <blair.seivers@yahoo.com>
2022-05-01 12:57:42 -0400

    add vote routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
+
 //create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -8,4 +10,34 @@ Post.belongsTo(User, {
     foeignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+//many-to-many association
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+  });
+
+  //one-to-many user vote associations
+  Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -61,6 +61,16 @@ router.get('/:id', (req, res) => {
       });
   });
 
+  // PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+      })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => res.json(err));
+});
+
   //updates existing post
   router.put('/:id', (req, res) => {
     Post.update({
